63d16b0f64b4d779ee6fadc530b3a15055201782
Ryan Wei <rainmanwy@gmail.com>
2025-05-06 14:29:03 +0800

    Add more information

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@
 
 logfilename=$(date +%Y%m%d)_$(date +%H%M%S)
 nohup sh tpcds.sh > tpcds_$logfilename.log 2>&1 &
+echo "Benchmark started running in the background, please check tpcds_$logfilename.log for more information."
+echo "To stop the benchmark, run: kill \$(ps -ef | grep tpcds.sh | grep -v grep | awk '{print \$2}')"
+echo "To check the status of the benchmark, run: tail -f tpcds_$logfilename.log"
