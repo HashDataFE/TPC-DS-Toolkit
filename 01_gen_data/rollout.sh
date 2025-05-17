931eb5b9f1f8f59b611a09b6d45de309bd5bfe05
RyanWei <rainmanwy@gmail.com>
2025-05-17 21:58:16 +0800

    debug

--- 01_gen_data/rollout.sh
+++ 01_gen_data/rollout.sh
@@ -65,10 +65,11 @@ function gen_data() {
     #CHILD=$(echo ${i} | awk -F '|' '{print $1}')
     EXT_HOST=$(echo ${i} | awk -F '|' '{print $2}')
     GEN_DATA_PATH=$(echo ${i} | awk -F '|' '{print $3}' | sed 's#//#/#g')
-    GEN_DATA_PATH="${GEN_DATA_PATH}/dsbenchmark/${CHILD}"
+    #GEN_DATA_PATH="${GEN_DATA_PATH}/dsbenchmark/${CHILD}"
     for ((j=1; j<=LOCAL_GEN_PARALLEL; j++)); do
       echo "ssh -n ${EXT_HOST} \"rm -rf /tmp/tpcds.generate_data*.log\""
       ssh -n ${EXT_HOST} "rm -rf /tmp/tpcds.generate_data*.log"
+      GEN_DATA_PATH="${GEN_DATA_PATH}/dsbenchmark/${CHILD}"
       echo "ssh -n ${EXT_HOST} \"bash -c 'cd ~/; ./generate_data.sh ${GEN_DATA_SCALE} ${CHILD} ${PARALLEL} ${GEN_DATA_PATH} > /tmp/tpcds.generate_data.${CHILD}.log 2>&1 &'\""
       ssh -n ${EXT_HOST} "bash -c 'cd ~/; ./generate_data.sh ${GEN_DATA_SCALE} ${CHILD} ${PARALLEL} ${GEN_DATA_PATH} > /tmp/tpcds.generate_data.${CHILD}.log 2>&1 &'" &
       CHILD=$((CHILD + 1))
