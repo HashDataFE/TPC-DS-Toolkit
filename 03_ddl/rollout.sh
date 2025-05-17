c8e0d366a6e64485e99dfe3a95c54aa1172ff7e9
RyanWei <rainmanwy@gmail.com>
2025-05-17 22:37:51 +0800

    bug fix for data loading in local mode

--- 03_ddl/rollout.sh
+++ 03_ddl/rollout.sh
@@ -90,7 +90,7 @@ if [ "${DROP_EXISTING_TABLES}" == "true" ]; then
             else
               LOCATION+="', '"
             fi
-              LOCATION+="gpfdist://${EXT_HOST}:${PORT}/${table_name}_[0-9]*_[0-9]*.dat"
+              LOCATION+="gpfdist://${EXT_HOST}:${PORT}/[0-9]*/${table_name}_[0-9]*_[0-9]*.dat"
               counter=$((counter + 1))
           done
           LOCATION+="'"
