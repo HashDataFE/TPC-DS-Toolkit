251166f514861149b4fce4f4aec8f7ffcaedfa9b
RyanWei <rainmanwy@gmail.com>
2025-05-23 17:33:09 +0800

    debug

--- tpcds.sh
+++ tpcds.sh
@@ -29,6 +29,7 @@ print_header
 get_version
 export DB_VERSION=${VERSION}
 export DB_FULL_VERSION=${FULL_VERSION}
+log_time "Current database running this test is:\n${VERSION_FULL}"
 
 # run the benchmark
 ./rollout.sh
