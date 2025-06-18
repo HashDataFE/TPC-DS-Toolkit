cffa12d96bebbc7ad650004f3f605b57eda3927f
Ryan Wei <rainmanwy@gmail.com>
2025-06-18 18:45:32 +0800

    debug

--- tpcds_variables.sh
+++ tpcds_variables.sh
@@ -2,7 +2,6 @@
 export ADMIN_USER="gpadmin"
 export BENCH_ROLE="dsbench"
 export DB_SCHEMA_NAME="tpcds"
-export GREENPLUM_PATH=$GPHOME/greenplum_path.sh
  
 ## Set to "local" to run the benchmark on the COORDINATOR host or "cloud" to run the benchmark from a remote client.
 export RUN_MODEL="cloud"
