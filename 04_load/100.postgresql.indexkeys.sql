ff216ac4f8a790f9bfe5d8177f732a22cb924b71
Ryan Wei <rainmanwy@gmail.com>
2025-07-10 11:12:03 +0800

    debug

--- 04_load/100.postgresql.indexkeys.sql
+++ 04_load/100.postgresql.indexkeys.sql
@@ -13,6 +13,7 @@ set search_path=:DB_SCHEMA_NAME,public;
 -- Fact Table Indexes
 -- ===================================================================
 
+
 -- 1. store_sales table indexes
 COMMENT ON TABLE store_sales IS 'Sales fact table (store channel)';
 -- Optimization: Merge redundant indexes, keep most effective composite indexes
@@ -33,6 +34,7 @@ CREATE INDEX idx_catalog_sales_item_date ON catalog_sales (cs_item_sk, cs_sold_d
 
 -- 4. Return fact table indexes
 CREATE INDEX idx_store_returns_sale ON store_returns (sr_item_sk, sr_ticket_number) INCLUDE (sr_returned_date_sk, sr_return_amt);
+CREATE INDEX idx_store_returns_date ON store_returns(sr_returned_date_sk);
 CREATE INDEX idx_web_returns_sale ON web_returns (wr_item_sk, wr_order_number) INCLUDE (wr_returned_date_sk, wr_return_amt);
 CREATE INDEX idx_catalog_returns_sale ON catalog_returns (cr_item_sk, cr_order_number) INCLUDE (cr_returned_date_sk, cr_return_amount);
 
@@ -62,6 +64,7 @@ CREATE INDEX idx_date_dim_year_month ON date_dim (d_year, d_moy, d_date_sk);
 CREATE INDEX idx_date_dim_quarter ON date_dim (d_qoy, d_year, d_date_sk);
 -- Add: Date range query optimization index
 CREATE INDEX idx_date_dim_range ON date_dim (d_date_sk, d_year, d_moy, d_dom);
+CREATE INDEX idx_date_dim_year ON date_dim(d_year);
 
 -- 4. store table
 COMMENT ON TABLE store IS 'Store dimension table';
@@ -69,6 +72,7 @@ CREATE UNIQUE INDEX idx_store_id ON store (s_store_sk);
 CREATE INDEX idx_store_address ON store (s_county, s_state, s_store_sk);
 -- Add: Store performance metrics index
 CREATE INDEX idx_store_perf ON store (s_store_sk, s_floor_space, s_number_employees); -- Use valid columns
+CREATE INDEX idx_store_state ON store(s_state);
 
 -- 5. web_page and catalog_page
 CREATE INDEX idx_web_page_url ON web_page (wp_web_page_sk, wp_access_date_sk, wp_type);
@@ -93,5 +97,6 @@ WHERE ss_sold_date_sk >= 2452275; -- Use the actual value from step 1
 
 -- Expression index
 CREATE INDEX idx_store_returns_amount_expr ON store_returns ((sr_return_amt * 0.9));
+CREATE INDEX idx_store_returns_date ON store_returns(sr_returned_date_sk);
 -- Prompt message
 \echo 'TPC-DS index creation completed!'
